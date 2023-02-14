acb088be9a7a0b140ee5c67cbbda0e832e7f602c
Pavankumar <pavankumar.kumar155@gmail.com>
2023-02-15 01:10:33 +0530

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import Header from './components/Header/Header';
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [books, setBooks] = useState([]);
+    const [resultTitle, setResultTitle] = useState([]);
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home setBooks={setBooks} setResultTitle={setResultTitle}/>} />
+                <Route path="/about" element={<About abt={true}/>} />
+                <Route path="/book" element={<><Header bk={true}/><BookList books={books} resultTitle={resultTitle}/></>} />
+                <Route path="/book/:id" element={<BookDetails books={books} />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
